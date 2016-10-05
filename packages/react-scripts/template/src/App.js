3fece23c74c3a775abab321ea9cb81a668f21458
Archie Lee <archielee@appier.com>
2016-10-05 15:39:37 +0800

    Add classnams library and use classnams' bind for CSS module

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import classnames from 'classnames/bind';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.css';
+
+const cx = classnames.bind(style);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={cx('App')}>
+        <div className={cx('App-header')}>
+          <img src={logo} className={cx('App-logo')} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className={cx('App-intro')}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
