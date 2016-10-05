cfbe3f273663125152c8d957d070c61a9fbe33c1
Archie Lee <archielee@appier.com>
2016-10-05 18:51:11 +0800

    Add Redux and related libraris

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import { AppContainer } from 'react-hot-loader';
 import App from './App';
+import createStore from './store'
 import './index.css';
+
+const store = createStore();
 
 ReactDOM.render(
   <AppContainer>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </AppContainer>,
   document.getElementById('root')
 );
@@ -14,10 +20,12 @@ ReactDOM.render(
 if (module.hot) {
   module.hot.accept('./App', () => {
     const NextApp = require('./App').default;
-    
+
     ReactDOM.render(
       <AppContainer>
-        <NextApp />
+        <Provider store={store}>
+          <NextApp />
+        </Provider>
       </AppContainer>,
       document.getElementById('root')
     );
