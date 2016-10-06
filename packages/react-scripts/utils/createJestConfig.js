1deaafe422884f4e31a20ad7c92b59126413a141
Archie Lee <archielee@appier.com>
2016-10-06 17:59:23 +0800

    Use named export to export App component for unit test

--- packages/react-scripts/utils/createJestConfig.js
+++ packages/react-scripts/utils/createJestConfig.js
@@ -21,6 +21,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
   }
 
   const config = {
+    modulePaths: ['<rootDir>/src'],
     moduleFileExtensions: ['jsx', 'js', 'json'],
     moduleNameMapper: {
       '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': resolve('config/jest/FileStub.js'),
