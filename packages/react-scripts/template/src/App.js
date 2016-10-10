807e21d981cf59379c3b5821f70a153d6b2c0363
Archie Lee <archielee@appier.com>
2016-10-10 11:12:14 +0800

    Add `Recompose` library

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,29 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { bindActionCreators, compose } from 'redux';
+import { bindActionCreators } from 'redux';
 import classnames from 'classnames/bind';
+import { pure, compose, setDisplayName } from 'recompose';
 import * as AuthActions from './ducks/auth';
 import logo from './logo.svg';
 import style from './App.css';
 
 const cx = classnames.bind(style);
+const enhanced = compose(
+  setDisplayName('App'),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  withRouter,
+  pure
+);
 
-export class App extends Component {
-  render() {
-    return (
-      <div className={cx('App')}>
-        <div className={cx('App-header')}>
-          <img src={logo} className={cx('App-logo')} alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className={cx('App-intro')}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+export const App = props => (
+  <div className={cx('App')}>
+    <div className={cx('App-header')}>
+      <img src={logo} className={cx('App-logo')} alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
+    <p className={cx('App-intro')}>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
 
 function mapStateToProps(state) {
   return {
@@ -39,10 +45,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default compose(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  ),
-  withRouter,
-)(App);
+export default enhanced(App);
