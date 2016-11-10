1bc4c089235defdb37e0930468abc1c625617b7e
Archie Lee <archielee@appier.com>
2016-11-10 11:12:02 +0800

    Add example app

---
+++ example-app/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hashHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux'
+import { AppContainer } from 'react-hot-loader';
+import Routes from './Routes';
+import createStore from './reducers/'
+import './index.css';
+
+const store = createStore();
+const history = syncHistoryWithStore(hashHistory, store);
+const containerNode = document.getElementById('root');
+
+ReactDOM.render(
+  <AppContainer>
+    <Provider store={store}>
+      <Routes history={history} />
+    </Provider>
+  </AppContainer>,
+  containerNode
+);
+
+if (module.hot) {
+  module.hot.accept('./Routes', () => {
+    const NextRoutes = require('./Routes').default;
+
+    ReactDOM.render(
+      <AppContainer>
+        <Provider store={store} history={history}>
+          <NextRoutes history={history} />
+        </Provider>
+      </AppContainer>,
+      containerNode
+    );
+  });
+}
