418a7056e9c2e027103eb9bd15c48b017f50ad8d
Archie Lee <archielee@appier.com>
2017-01-17 17:42:03 +0800

    Fix storybook HRM broken

--- example-app/.storybook/webpack.config.js
+++ example-app/.storybook/webpack.config.js
@@ -1,1 +1,12 @@
-module.exports = require('react-scripts-appier/config/webpack.config.dev.js');
+const basicConfig =require('react-scripts-appier/config/webpack.config.dev.js');
+
+module.exports = function(config, env) {
+  const [asset, js, cssIn, cssEx, json, svg] = baseConfig.module.loaders;
+
+  // Don't add CRA's "asset" loader, it'll break the storybook HMR
+  config.module.loaders = config.module.loaders.concat([cssIn, cssEx, json, svg]);
+  config.postcss = baseConfig.postcss;
+
+  return config;
+};
+
