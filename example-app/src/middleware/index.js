a8de7d017ec18a77791cd609fd36d68eecf367cf
Archie Lee <archielee@appier.com>
2017-02-26 11:28:51 +0800

    Delete `example-app` folder to reduce diff

--- example-app/src/middleware/index.js
+++
@@ -1,8 +0,0 @@
-import thunk from 'redux-thunk';
-import { routerMiddleware } from 'react-router-redux';
-import { hashHistory } from 'react-router';
-
-const router = routerMiddleware(hashHistory);
-const middlerwares = [thunk, router];
-
-export default middlerwares;
