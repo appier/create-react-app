a58b8d7803aa4a439670674bcef1709ddf7dae23
Archie Lee <archielee@appier.com>
2017-06-20 15:40:11 +0800

    Use absolute path instead of relative path

--- packages/react-scripts/template/src/reducers/index.js
+++ packages/react-scripts/template/src/reducers/index.js
@@ -2,16 +2,14 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { routerReducer } from 'react-router-redux';
 import auth from './auth';
-import middleware from '../middleware/';
+import middleware from 'middleware';
 
 const reducers = combineReducers({
   routing: routerReducer,
   auth,
 });
 
-const enhancer = composeWithDevTools(
-  applyMiddleware(...middleware)
-);
+const enhancer = composeWithDevTools(applyMiddleware(...middleware));
 
 export default function configure(initialState) {
   const store = createStore(reducers, initialState, enhancer);
